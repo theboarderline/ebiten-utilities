3de48f7fb388ec91b8588583c556ae95c30febc6
Walker O'Brien <walkernobrien@gmail.com>
2023-10-07 00:05:31 -0500

    add more snake utilities

---
+++ snake/object/snake/distance.go
@@ -0,0 +1,112 @@
+package snake
+
+import (
+	c "github.com/theboarderline/ebiten-utilities/snake/core"
+	"github.com/theboarderline/ebiten-utilities/snake/object"
+	"github.com/theboarderline/ebiten-utilities/snake/param"
+	"math"
+)
+
+func (s *Snake) checkSnakeIntersection() {
+	curUnit := s.UnitHead.Next
+	if curUnit == nil {
+		return
+	}
+
+	var tolerance float32 = param.ToleranceDefault
+	if len(curUnit.CompCollision.Rects) > 1 { // If second unit is on an edge
+		tolerance = param.ToleranceScreenEdge // To avoid false collisions on screen edges
+	}
+
+	for curUnit != nil {
+		if object.Collides(s.UnitHead, curUnit, tolerance) {
+			return
+		}
+		curUnit = curUnit.Next
+	}
+}
+
+func (s *Snake) calcSnakeFoodDist(foodLoc c.Vec64) float32 {
+	headLoc := s.UnitHead.HeadCenter
+
+	minDist := c.Distance(headLoc, foodLoc)
+
+	if headLoc.X < param.HalfScreenWidth { // Left projection distance
+		virtualFood := c.Vec64{X: foodLoc.X - param.ScreenWidth, Y: foodLoc.Y}
+		minDist = math.Min(minDist, c.Distance(headLoc, virtualFood))
+	} else if headLoc.X >= param.HalfScreenWidth { // Right projection distance
+		virtualFood := c.Vec64{X: foodLoc.X + param.ScreenWidth, Y: foodLoc.Y}
+		minDist = math.Min(minDist, c.Distance(headLoc, virtualFood))
+	}
+
+	if headLoc.Y < param.HalfScreenHeight { // Upper projection distance
+		virtualFood := c.Vec64{X: foodLoc.X, Y: foodLoc.Y - param.ScreenHeight}
+		minDist = math.Min(minDist, c.Distance(headLoc, virtualFood))
+	} else if headLoc.Y >= param.HalfScreenHeight { // Bottom projection distance
+		virtualFood := c.Vec64{X: foodLoc.X, Y: foodLoc.Y + param.ScreenHeight}
+		minDist = math.Min(minDist, c.Distance(headLoc, virtualFood))
+	}
+
+	return float32(minDist)
+}
+
+func (s *Snake) handleDirectionInput(direction DirectionT) {
+	pressedLeft := direction == DirectionLeft
+	pressedRight := direction == DirectionRight
+	pressedUp := direction == DirectionUp
+	pressedDown := direction == DirectionDown
+
+	if !pressedLeft && !pressedRight && !pressedUp && !pressedDown {
+		return
+	}
+
+	dirCurrent := s.LastDirection()
+	dirNew := dirCurrent
+
+	if dirCurrent.IsVertical() {
+		if pressedLeft {
+			dirNew = DirectionLeft
+		} else if pressedRight {
+			dirNew = DirectionRight
+		}
+	} else {
+		if pressedUp {
+			dirNew = DirectionUp
+		} else if pressedDown {
+			dirNew = DirectionDown
+		}
+	}
+
+	if dirNew == dirCurrent {
+		return
+	}
+
+	newTurn := NewTurn(dirCurrent, dirNew)
+	s.TurnTo(newTurn, false)
+}
+
+func (s *Snake) checkFoodCollision(distToFood float32) bool {
+	if distToFood <= param.RadiusEating {
+		s.Grow()
+		return true
+	}
+
+	return false
+}
+
+func (s *Snake) triggerScoreAnim() *object.ScoreAnim {
+	corrCenter := s.UnitHead.HeadCenter
+
+	switch s.UnitHead.Direction {
+	case DirectionUp:
+		corrCenter.Y -= param.RadiusSnake
+	case DirectionDown:
+		corrCenter.Y += param.RadiusSnake
+	case DirectionRight:
+		corrCenter.X += param.RadiusSnake
+	case DirectionLeft:
+		corrCenter.X -= param.RadiusSnake
+	}
+
+	return object.NewScoreAnim(corrCenter.To32())
+}
